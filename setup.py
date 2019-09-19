9865924bfdafed0db42b444d6d2117efd947c0aa
Niklas Beuster <niklas.beuster@tu-ilmenau.de>
2019-09-19 19:41:26 +0200

    Added package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='g2c1',
+    version='0.1',
+    description='Generating and parsing UHF RFID Gen 2 Class 1 messages',
+    url='https://csgitlab.tu-ilmenau.de/nibe8075/g2c1messages',
+    author='Niklas Beuster',
+    author_email='niklas.beuster@tu-ilmenau.de',
+    license='MIT',
+    packages=['g2c1messages'],
+    zip_safe=False)
