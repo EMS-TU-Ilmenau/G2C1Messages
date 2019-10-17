59d415340c6ab03b9f0ee76ca9cecf7883681e21
Niklas <niklas.beuster@tu-ilmenau.de>
2019-10-17 13:34:30 +0200

    updated project url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='g2c1',
     version='0.1',
     description='Generating and parsing UHF RFID Gen 2 Class 1 messages',
-    url='https://csgitlab.tu-ilmenau.de/nibe8075/g2c1messages',
+    url='https://gitlab.tu-ilmenau.de/FakEI/InIT/it-ems/Projects/vabuleus/interrogator/g2c1messages',
     author='Niklas Beuster',
     author_email='niklas.beuster@tu-ilmenau.de',
     license='MIT',
